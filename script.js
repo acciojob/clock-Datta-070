3f95e631bfbf44c121e245d3c2c652c2840e3c01
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-11-21 09:07:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //your JS code here. If required.
 function display_ct5(){
-	let x= new Date();
-	let ampm= x.getHours() >= 12 ? 'PM':'AM';
-	let x1= x.getMonth() + 1 + "/" + x.getDate() + "/" + x.getFullyear();
+	let x= new Date()
+	let ampm= x.getHours() >= 12 ? ' PM':' AM';
+	let x1= x.getMonth() + 1 + "/" + x.getDate() + "/" + x.getFullYear();
 	x1= x1 + ", " + x.getHours() + ":" + x.getMinutes() + ":" + x.getSeconds() +" "+ ampm;
 	document.getElementById('timer').innerHTML=x1;
 	display_c5();
 }
 funtion display_c5(){
 	let refresh=1000;
-	mytime=setTimeout(display(), refresh);
+	let mytime=setTimeout('display_ct5()', refresh)
 }
-display_c5();
+display_c5()
